3729310da146183d895200a7790b24dba757265d
Lu4nL1ma <luanlima.corp@gmail.com>
2025-01-28 19:33:44 -0300

    changes_01

--- dae_sys/dae_web_sys/models.py
+++ dae_sys/dae_web_sys/models.py
@@ -12,3 +12,16 @@ class regiao(models.Model):
     regiao = models.CharField(max_length=40, default='')
     def __str__(self) -> str:
         return self.regiao
+
+class custos(models.Model):
+    mesorreg = models.CharField(max_length=40, default='')
+    regint = models.CharField(max_length=40, default='')
+    municipio = models.CharField(max_length=40, default='')
+    modalidade = models.CharField(max_length=40, default='')
+    tecnologia = models.CharField(max_length=40, default='')
+    mbps = models.CharField(max_length=40, default='')
+    cunittransp = models.FloatField(default=0.0)
+    cmanut = models.FloatField(default=0.0)
+
+    def __str__(self) -> str:
+        return self.custos
--- dae_sys/dae_web_sys/tests.py
+++ dae_sys/dae_web_sys/tests.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 
 #ler dados
-dados = pd.read_excel('municipios_para.xlsx')
+dados = pd.read_excel('custos.xlsx')
 
 df = pd.DataFrame(dados)
 
@@ -12,7 +12,7 @@ conn = sqlite3.connect('C:\\Users\\corpl\\OneDrive\\DA3_SYSTEMS\\dae_sys\\db.sql
 cursor = conn.cursor()
 
 # Inserindo os dados do DataFrame na tabela 'pessoas' (assumindo que ela já existe)
-df.to_sql('dae_web_sys_regiao_municipio', conn, if_exists='append', index=False)
+df.to_sql('dae_web_sys_custos', conn, if_exists='append', index=False)
 
 # # # Listando todas as tabelas
 # # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
