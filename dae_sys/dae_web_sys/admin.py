f06515bd3e56b957a4626192ff27c59bf1cf8058
Lu4nL1ma <luanlima.corp@gmail.com>
2025-01-29 23:43:58 -0300

    fds

--- dae_sys/dae_web_sys/admin.py
+++ dae_sys/dae_web_sys/admin.py
@@ -5,11 +5,17 @@ from dae_web_sys import models
 @admin.register(models.regiao)
 class regiao(admin.ModelAdmin):
     list_display = 'id','regiao'
+    class Meta:
+        verbose_name = "Regiõe"
 
 @admin.register(models.regiao_municipio)
 class regiao_municipio(admin.ModelAdmin):
     list_display = 'id', 'regiao', 'municipio'
+    class Meta:
+        verbose_name = "Município"
 
 @admin.register(models.custos)
 class custos(admin.ModelAdmin):
     list_display = 'id', 'mesorreg' , 'regint' , 'municipio', 'modalidade', 'tecnologia','cunittransp', 'cmanut'
+    class Meta:
+        verbose_name = "Custo"
