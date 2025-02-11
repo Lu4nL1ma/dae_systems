7cd01de6376ce364c58cb5fe5f3ccbc2f034d387
Lu4nL1ma <luanlima.corp@gmail.com>
2025-02-10 21:59:04 -0300

    Sua mensagem de commit descritiva

--- dae_sys/dae_web_sys/utils/functions.py
+++ dae_sys/dae_web_sys/utils/functions.py
@@ -5,18 +5,3 @@ def cal_pref(row):
 
     return somar
                 
-def atualizar(row, mb_link):
-                        
-    row['cunittransp'] = float((row['cunittransp'])) * int(mb_link)
-
-    atuali = round(row['cunittransp'], 2)
-
-    return atuali
-                
-def atualizar_mbps(row, mb_link):
-
-    row['mbps'] = mb_link
-
-    atuali = row['mbps']
-                        
-    return atuali
